3ce7d038b6968c535229580d86de8c2ca56a433a
Hy Huynh <hyxhuynh@gmail.com>
2019-06-04 17:24:01 -0700

    Attached correct properties to an image displayed in FlipView

--- ImagePage.xaml.cs
+++ ImagePage.xaml.cs
@@ -28,34 +28,57 @@ namespace PictureApp
             this.InitializeComponent();
         }
 
+        // FLIPVIEW
+        // EventArgs e is a parameter called e that contains the event data, see the EventArgs MSDN page for more information.
+        // Object Sender is a parameter called Sender that contains a reference to the control/object that raised the event
+        // E.g. When Button is clicked, the btn_Click event handler will be fired.The "object sender" portion will be a reference to the button which was clicked
         private void MyFlipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-    
+            // Get the current flip view
+            FlipView fv = ((FlipView)sender);
+
+            // If the current flipview doesnt have a selected item just dont do anything
+            if( fv.SelectedItem == null )
+            {
+                return;
+            }
+
+            // When a selection is changed we need to change the displayed image discriptions as well
+            ImageItem selectedImage = fv.SelectedItem as ImageItem;
+            DisplayImageName.Text = "Name: " + selectedImage.ImageName;
+            DisplayFileType.Text = "Type: " + selectedImage.FileType;
+            DisplayFileSize.Text = "Size: " + selectedImage.Size.ToString() + " bytes";
+            DisplayDateModified.Text = "Date Modified: " + selectedImage.DateModified.ToString();
+
         }
+
+       
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+ 
             List<ImageItem> list1 = e.Parameter as List<ImageItem>;
-            // ImageItem displayitem = e.Parameter as ImageItem;
            
+            // List of images selected
             MyFlipView1.ItemsSource = list1;
-            //var filedetails = MyFlipView1.ItemsSource;
+
+            // Sets the current display text
             ImageItem imgItem = MainPage.displayitem;
-   
-           
-         
+            DisplayImageName.Text = "Name: " + imgItem.ImageName;
+            DisplayFileType.Text = "Type: " + imgItem.FileType;
+            DisplayFileSize.Text = "Size: " + imgItem.Size.ToString() + "bytes";
+            DisplayDateModified.Text = "Date Modified: " + imgItem.DateModified.ToString();
+
+
+            // Setting the currently selected item in the flipview to the MainPage.displayitem
+            // This is so when the user clicks on the displayed image in the mainpage, 
+            // it will select that image on the flip view in the image page
             foreach (ImageItem i in list1)
             {
-
-                while (imgItem.ImageName.Equals(i.ImageName))
+                if (imgItem.ImageName.Equals(i.ImageName))
                 {
                     MyFlipView1.SelectedItem = i;
-                    DisplayImageName.Text = "Name: " + i.ImageName;
-                    DisplayFileType.Text = "Type: " + i.FileType;
-                    DisplayFileSize.Text = "Size: " + i.Size.ToString();
-                    DisplayDateModified.Text = "Date Modified: " + i.DateModified.ToString();
-                    break;
                 }
-
             }
 
             base.OnNavigatedTo(e);
